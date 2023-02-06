e1907351f84052cfc55be28704b744737c6353ae
alessandroquirile <a.quirile@studenti.unina.it>
2023-02-06 19:41:00 +0100

    Minor refactoring

--- main.jl
+++ main.jl
@@ -5,33 +5,32 @@ include("dataset_injection.jl")
 include("rs.jl")
 include("dataset_analysis.jl")
 
+# Data injection
 linksDataFrame, moviesDataFrame, ratingsDataFrame, tagsDataFrame = loadData("ml-latest-small")
 
-numberOfMovies = size(moviesDataFrame, 1) # number of rows in moviesDataFrame
-numberOfRatings = size(ratingsDataFrame, 1) # number of rows in ratingsDataFrame
-numberOfUsers = length(unique(ratingsDataFrame[:,1])) # number of unique "userId" values in ratingsDataFrame
+numberOfMovies = size(moviesDataFrame, 1) # Number of rows in moviesDataFrame
+numberOfRatings = size(ratingsDataFrame, 1) # Number of rows in ratingsDataFrame
+numberOfUsers = length(unique(ratingsDataFrame[:,1])) # Number of unique "userId" values in ratingsDataFrame
 
+# Building the URM
 println("Building the User Rating Matrix...")
 URM = buildURM()
-
 density = numberOfRatings / (numberOfMovies * numberOfUsers)
 println("Loaded $numberOfMovies movies and $numberOfUsers users")
 println("Found $numberOfRatings ratings, URM matrix density is $(round(density*100, digits=3))%")
 
-
-#Print the Value of votes histogram
+# Value of votes histogram
 valueOfVotes = histogram(ratingsDataFrame.rating, title="Value of votes")
-#plot(valueOfVotes, show=true)
 
-
-#Arithmetic average
+# Arithmetic average
 moviesRatingAverage = getRoundedAverage(ratingsDataFrame, :movieId)
 arithmeticAverageHistogram = histogram(moviesRatingAverage.rating_function, title="Arithmetic average") # TODO: change x axis step size
 
-
+# Standard deviation
 moviesRatingStdDev = getRoundedStdDev(ratingsDataFrame, :movieId, true)
 stdDevHistogram = histogram(moviesRatingStdDev.rating_function, title="Standard deviation") # TODO: change x axis step size
 
+# Plotting
 showHistogram(valueOfVotes)
 showHistogram(arithmeticAverageHistogram)
 showHistogram(stdDevHistogram)
