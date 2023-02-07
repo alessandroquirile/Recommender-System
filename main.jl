804738413f4901f119f92c303f80d652e895f1cf
alessandroquirile <a.quirile@studenti.unina.it>
2023-02-07 18:30:26 +0100

    Minor refactoring

--- main.jl
+++ main.jl
@@ -13,27 +13,24 @@ moviesDataFrame, ratingsDataFrame = loadDataSlim("ml-latest-small")
 numberOfUsers = length(unique(ratingsDataFrame[:, 1])) # Number of unique "userId" values in ratingsDataFrame
 numberOfMovies = size(moviesDataFrame, 1) # Number of rows in moviesDataFrame
 
-
+# Print data set statistics
 printStatistics()
 
-# Splitting in training and test set
+# Training and test set splitting
 testSetSize = 0.10
 trainingAndValidationDataFrame, testDataFrame = kFoldSplit(ratingsDataFrame, numberOfUsers, numberOfMovies, testSetSize, 0)
-
 
 for kFoldIndex = 0:9
     println("Running fold number $kFoldIndex")
 
+    # Training and validation set splitting
     validationSetSize = 0.10
     trainingDataFrame, validationDataFrame = kFoldSplit(trainingAndValidationDataFrame, numberOfUsers, numberOfMovies, validationSetSize, kFoldIndex)
 
     # Building the URM
     trainingURM = buildURM(trainingDataFrame, numberOfUsers, numberOfMovies)
 
+    # Printing info
     printInfo(trainingURM)
     printDensity(trainingURM, trainingDataFrame)
 end
-
-
-
-
