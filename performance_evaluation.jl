4eb156d45c369d15c12daa909f1b500e4d4c9634
imatrisciano <ivanomatrisciano@gmail.com>
2023-02-09 15:57:26 +0100

    Added progress report to computeModelError

--- performance_evaluation.jl
+++ performance_evaluation.jl
@@ -4,7 +4,12 @@ function computeModelError(trainingURM, targetDataFrame, targetURM, aggregationM
     targets = targetDataFrame[:, :rating]
     normalize!(targets, getRatingRange())
 
+    counter = Threads.Atomic{Int64}(0) # counter is an atomic variable of type Int64 initialized to 0
     @threads for i = 1:testSetItemCount
+        Threads.atomic_add!(counter, 1) # increment counter
+        progressPercentage = round(counter.value/testSetItemCount*100, digits=2)
+        print("\rComputing prediction $(counter.value)/$testSetItemCount ($progressPercentage%)    ")
+
         userId = targetDataFrame[i, :userId]
         movieId = targetDataFrame[i, :movieId]
         item = getMovieIndexById(movieId)
@@ -15,7 +20,8 @@ function computeModelError(trainingURM, targetDataFrame, targetURM, aggregationM
         # Explicit call to the Garbage Collecter to fix a memory leak
         GC.gc(false) # fullSweep = false means the GC will only clear "young" objects, speeding up the collection
     end
-    
+    println("") # newline after progress report
+
     error = errorFunction(targets, predictions)
     return error
 end
